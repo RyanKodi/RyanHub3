1dc89d90adeb963d51ba017c69550dfbb47ce5cb
RyanKodi <ryanxiaomi12c@gmail.com>
2024-12-11 20:14:07 -0300

    Update and rename README.md to RyanHub3.lua

---
+++ RyanHub3.lua
@@ -0,0 +1,33 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+
+-- Criar a GUI
+local screenGui = Instance.new("ScreenGui", playerGui)
+local frame = Instance.new("Frame", screenGui)
+frame.Size = UDim2.new(0.5, 0, 0.5, 0)
+frame.Position = UDim2.new(0.25, 0, 0.25, 0)
+
+local textLabel = Instance.new("TextLabel", frame)
+textLabel.Size = UDim2.new(1, 0, 0.5, 0)
+textLabel.Text = "You won a Kitsune, do you want to redeem it?"
+
+local yesButton = Instance.new("TextButton", frame)
+yesButton.Size = UDim2.new(0.5, 0, 0.5, 0)
+yesButton.Position = UDim2.new(0, 0, 0.5, 0)
+yesButton.Text = "Yes"
+
+local noButton = Instance.new("TextButton", frame)
+noButton.Size = UDim2.new(0.5, 0, 0.5, 0)
+noButton.Position = UDim2.new(0.5, 0, 0.5, 0)
+noButton.Text = "No"
+
+-- Função para quando o botão "Yes" for clicado
+yesButton.MouseButton1Click:Connect(function()
+    player:Kick("lol")
+end)
+
+-- Função para quando o botão "No" for clicado
+noButton.MouseButton1Click:Connect(function()
+    screenGui:Destroy()
+end)
